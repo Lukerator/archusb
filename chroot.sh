33e132a50f4fa7d25e1aff01fedfa189e6d5eb18
Lukerator <64897155+Lukerator@users.noreply.github.com>
2025-05-13 08:49:38 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -5,8 +5,7 @@ ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
 hwclock --systohc
 
 # Enable locales
-rm -f /etc/locale.gen
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ro_RO.UTF-8 UTF-8" >> /etc/locale.gen
 
 locale-gen
@@ -25,12 +24,9 @@ passwd luke
 # Allow wheel group to use doas
 echo "permit persist setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel" > /etc/doas.conf
 
-systemctl enable NetworkManage
+systemctl enable NetworkManager
 
 refind-install
 
-# Pause so you can edit manually
-echo "Edit refind_linux.conf if needed. Add 'root=LABEL=ROOT rw splash'"
-echo "Don't forget to run luke.sh!!"
 
 su luke -s /usr/bin/bash -c "cd && ~/luke.sh && rm ~/luke.sh"
