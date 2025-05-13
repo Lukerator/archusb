56e41d65d3d41c1617924390e427c3c5e27132d6
Lukerator <64897155+Lukerator@users.noreply.github.com>
2025-05-13 16:10:48 +0300

    Rewrite arch.sh

--- arch.sh
+++ arch.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 set -e
 
+# Format partitions and create labels
 mkfs.fat -F32 /dev/nvme0n1p1
 fatlabel /dev/nvme0n1p1 BOOT
 mkfs.btrfs -f /dev/nvme0n1p2 -L ROOT
 
+# Mount the root partition and create subvolumes
 mount /dev/nvme0n1p2 /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
 btrfs su cr /mnt/@snapshots
 umount /mnt
 
+# Mount root subvolume
 mount -o noatime,compress=zstd,subvol=@ /dev/nvme0n1p2 /mnt
 
+# Create directories for boot, home, and snapshots
 mkdir -p /mnt/{boot,home,.snapshots}
 
+# Mount @home and @snapshots subvolumes
 mount -o noatime,compress=zstd,subvol=@home /dev/nvme0n1p2 /mnt/home
 mount -o noatime,compress=zstd,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
 
+# Mount the EFI partition
 mount /dev/nvme0n1p1 /mnt/boot
 
+# Install base system packages
 pacstrap -K /mnt \
   base base-devel linux linux-firmware \
   zsh \
@@ -30,11 +37,14 @@ pacstrap -K /mnt \
   intel-ucode sof-firmware \
   git neovim
 
+# Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# Set executable permissions for post-install scripts
 chmod +x chroot.sh
 chmod +x luke.sh
 cp chroot.sh /mnt/chroot.sh
 cp luke.sh /mnt/luke.sh
 
-arch-chroot /mnt /chroot.zh
+# Chroot into the new system
+arch-chroot /mnt /chroot.sh
