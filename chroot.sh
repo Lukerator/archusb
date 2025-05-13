4ada9b4edbd53b8d79127dd550dc2ab0d741ddd6
Lukerator <64897155+Lukerator@users.noreply.github.com>
2025-05-13 15:38:51 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -28,8 +28,16 @@ systemctl enable NetworkManager
 
 refind-install
 
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
 echo "" >> /etc/pacman.conf
 echo "[cahotic-aur]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
+pacman -Syu --noconfirm paru
 
 su luke -s /usr/bin/bash -c "cd && ~/luke.sh && rm ~/luke.sh"
