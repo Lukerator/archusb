815519f026c116cb0e304720d12bbec600e9e4bf
Lukerator <64897155+Lukerator@users.noreply.github.com>
2025-05-13 15:54:25 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -5,8 +5,20 @@ mkfs.fat -F32 /dev/nvme0n1p1
 fatlabel /dev/nvme0n1p1 BOOT
 mkfs.btrfs -f /dev/nvme0n1p2 -L ROOT
 
-mount -o noatime,compress=zstd,autodefrag /dev/nvme0n1p2 /mnt
-mount --mkdir /dev/nvme0n1p1 /mnt/boot
+mount /dev/nvme0n1p2 /mnt
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@snapshots
+umount /mnt
+
+mount -o noatime,compress=zstd,subvol=@ /dev/nvme0n1p2 /mnt
+
+mkdir -p /mnt/{boot,home,.snapshots}
+
+mount -o noatime,compress=zstd,subvol=@home /dev/nvme0n1p2 /mnt/home
+mount -o noatime,compress=zstd,subvol=@snapshots /dev/nvme0n1p2 /mnt/.snapshots
+
+mount /dev/nvme0n1p1 /mnt/boot
 
 pacstrap -K /mnt \
   base base-devel linux linux-firmware \
@@ -18,7 +30,7 @@ pacstrap -K /mnt \
   intel-ucode sof-firmware \
   git neovim
 
-genfstab -L /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 
 chmod +x chroot.sh
 chmod +x luke.sh
